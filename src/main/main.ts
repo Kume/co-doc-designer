cbea4ab8e34424de0cf3308a35c604ab93c46cfc
makoto.kume <makoto.kume07@gmail.com>
2019-04-14 23:03:48 +0900

    メニューの追加

--- src/main/main.ts
+++ src/main/main.ts
@@ -1,4 +1,5 @@
-import { app, BrowserWindow } from 'electron';
+import { app, BrowserWindow, Menu } from 'electron';
+import * as _ from 'underscore';
 
 const isDevelopment = process.env.NODE_ENV !== 'production';
 
@@ -7,6 +8,28 @@ app.on('window-all-closed', () => {
 });
 
 app.on('ready', function() {
+  const template = [{
+    label: 'Application',
+    submenu: [
+      { role: 'quit' }
+    ]}, {
+    label: 'Edit',
+    submenu: [
+      { role: 'cut' },
+      { role: 'copy' },
+      { role: 'paste' },
+      { label: 'Select All', accelerator: 'CmdOrCtrl+A', selector: 'selectAll:' },
+    ]},
+    {
+      label: 'View',
+      submenu: _.compact([
+        isDevelopment && { role: 'toggledevtools' },
+      ])
+    },
+  ];
+
+  Menu.setApplicationMenu(Menu.buildFromTemplate(template));
+
   let mainWindow: BrowserWindow | null = new BrowserWindow({width: 800, height: 600});
   const url = isDevelopment
     ? `http://localhost:${process.env.ELECTRON_WEBPACK_WDS_PORT}`
